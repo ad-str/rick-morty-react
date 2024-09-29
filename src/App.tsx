bb4e989952256c254d0db0f2469ce13527e0a8f2
Adam Streff <adam.michael.streff@gmail.com>
2024-09-29 12:30:14 -0400

    moved api call out of App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,12 @@
-import { useState, useEffect } from 'react';
 import './App.css';
-import { fetchData } from './api/api.ts';
+
 import FilterablePostsFeed from './components/FilterablePostsFeed.tsx';
-import { Character } from './types/types.ts';
 
 export default function App() {
-  const [characters, setCharacters] = useState<Character[]>([]);
-  const [error, setError] = useState<string | null>(null);
-
-  useEffect(() => {
-      const fetchCharacters = async () => {
-          try {
-              const data = await fetchData<{ results: Character[] }>('https://rickandmortyapi.com/api/character');
-              setCharacters(data);
-          } catch (err) {
-              setError((err as Error).message);
-          }
-      };
-
-      fetchCharacters();
-  }, []); // Empty dependency array means this runs once after the initial render
-
-
-  if (error) {
-    return <div>Error fetching characters: {error}</div>; // Show error message
-  }
   
   return (
     <div className="App">
-      <FilterablePostsFeed characters={characters} />
+      <FilterablePostsFeed />
     </div>
   );
 }
--- src/components/FilterablePostsFeed.tsx
+++ src/components/FilterablePostsFeed.tsx
@@ -1,42 +1,75 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { fetchData } from '../api/api.ts';
 import './FilterablePostsFeed.css';
 import CharacterCard from './CharacterCard';
 import { Character } from '../types/types';
 
-// specific types + interfaces
+// specific types
 type SortOrder = 'name' | 'date-new' | 'date-old';
+type StatusFilter = 'all' | 'alive' | 'dead' | 'unknown'
 
-interface StatusFilter {
-    alive: boolean;
-    dead: boolean;
-    unknown: boolean;
-}
-
-// interfaces for props
-interface FitlerablePostsFeedProps {
-    characters: Character[];
-}
-
+// interfaces for component props
 interface FilterBarProps {
-  sortOrder: SortOrder;
-  setSortOrder: React.Dispatch<React.SetStateAction<SortOrder>>;
-  statusFilter: StatusFilter;
-  setStatusFilter: React.Dispatch<React.SetStateAction<StatusFilter>>;
+    sortOrder: SortOrder;
+    setSortOrder: React.Dispatch<React.SetStateAction<SortOrder>>;
+    statusFilter: StatusFilter;
+    setStatusFilter: React.Dispatch<React.SetStateAction<StatusFilter>>;
 }
 
 interface PostsFeedProps {
     characters: Character[],
-    sortOrder: SortOrder,
-    statusFilter: StatusFilter
+    isLoading: boolean,
+    error: string | null
 }
 
-export default function FilterablePostsFeed({ characters }: FitlerablePostsFeedProps) {
+// main function
+export default function FilterablePostsFeed() {
     const [sortOrder, setSortOrder] = useState<SortOrder>('date-old'); // default to oldest to newest sorting
-    const [statusFilter, setStatusFilter] = useState<StatusFilter>({
-        alive: true,
-        dead: true,
-        unknown: true
-    }); // default to all checked
+    const [statusFilter, setStatusFilter] = useState<StatusFilter>('all'); // default to all
+    const [characters, setCharacters] = useState<Character[]>([]);
+    const [isLoading, setIsLoading] = useState<boolean>(true);
+    const [error, setError] = useState<string | null>(null);
+
+    useEffect(() => {
+        const fetchCharacters = async () => {
+            setIsLoading(true);
+            setError(null);
+            try {
+                // base url
+                let url = `https://rickandmortyapi.com/api/character`;
+                
+                // set status query
+                if (statusFilter !== 'all') {
+                    url += `?status=${statusFilter}`;
+                }
+
+                // fetch characters
+                let characters = await fetchData<{ results: Character[] }>(url);
+
+                // now sort results
+                characters.sort((a, b) => {
+                    switch (sortOrder) {
+                        case 'name':
+                            return a.name.localeCompare(b.name);
+                        case 'date-new':
+                            return b.created.getTime() - a.created.getTime();
+                        case 'date-old':
+                            return a.created.getTime() - b.created.getTime();
+                        default:
+                            return 0; // shouldn't reach this code
+                    }
+                })
+                
+                setCharacters(characters);
+            } catch (err) {
+                setError((err as Error).message);
+            } finally {
+                setIsLoading(false);
+            }
+        };
+
+        fetchCharacters();
+    }, [sortOrder, statusFilter]);
 
     return (
         <div>
@@ -48,8 +81,8 @@ export default function FilterablePostsFeed({ characters }: FitlerablePostsFeedP
             />
             <PostsFeed
                 characters={characters}
-                sortOrder={sortOrder}
-                statusFilter={statusFilter}
+                isLoading={isLoading}
+                error={error}
             />
         </div>
     );
@@ -60,12 +93,9 @@ function FilterBar({ sortOrder, setSortOrder, statusFilter, setStatusFilter }: F
         setSortOrder(event.target.value as SortOrder);
     };
     
-    const handleStatusChange = (status: keyof StatusFilter) => {
-        setStatusFilter(prev => ({
-            ...prev,
-            [status]: !prev[status]
-        }));
-      };
+    const handleStatusChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setStatusFilter(event.target.value as StatusFilter);
+    };
 
     return (
         <div className='filter-bar'>
@@ -79,51 +109,34 @@ function FilterBar({ sortOrder, setSortOrder, statusFilter, setStatusFilter }: F
             </div>
             
             <div className='status-container'>
-                <label>Status:</label>
-                {(Object.keys(statusFilter) as Array<keyof StatusFilter>).map(status => (
-                    <label key={status}>
-                        <input 
-                        type="checkbox" 
-                        name="status" 
-                        value={status}
-                        checked={statusFilter[status]}
-                        onChange={() => handleStatusChange(status)}
-                        />
-                        {status.charAt(0).toUpperCase() + status.slice(1)}
-                    </label>
-                ))}
+                <label htmlFor="status">Status: </label>
+                <select id="status" name="status" value={statusFilter} onChange={handleStatusChange}>
+                    <option value="all">All</option>
+                    <option value="alive">Alive</option>
+                    <option value="dead">Dead</option>
+                    <option value="unknown">Unknown</option>
+                </select>
             </div>
         </div>
     );
 };
 
-function PostsFeed({ characters, sortOrder, statusFilter }: PostsFeedProps) {
-    // filter the characters by status
-    const filteredCharacters = characters.filter(character => {
-        return statusFilter[character.status.toLowerCase() as keyof StatusFilter];
-    });
+function PostsFeed({ characters, isLoading, error }: PostsFeedProps) {
 
-    // now sort the filtered characters
-    const sortedCharacters = filteredCharacters.sort((a, b) => {
-        switch (sortOrder) {
-            case 'name':
-                return a.name.localeCompare(b.name);
-            case 'date-new':
-                return b.created.getTime() - a.created.getTime();
-            case 'date-old':
-                return a.created.getTime() - b.created.getTime();
-            default:
-                return 0;
-        }
-    });
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
 
+    if (error) {
+        return <div>Error: {error}</div>;
+    }
 
     return (
         <div className="feed-container">
-            {sortedCharacters.length === 0 ? (
-                <div>No characters found</div> // Display message if empty
+            {characters.length === 0 ? (
+                <div>No characters found</div>
             ) : (
-                sortedCharacters.map((character) => (
+                characters.map((character) => (
                     <CharacterCard character={character} />
                 ))
             )}
